3b1f755e47607e7a77896a095bd004d713e33104
Dave O <david.olivier@tech.geocent.com>
2020-05-21 17:53:45 -0500

    more game stuff

--- src/App.js
+++ src/App.js
@@ -6,14 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="Appz">
-      <header className="App-headerz">
-        <img src={easter_egg} className="App-logo" alt="logo" />
-        <p>
-          Congratulations, you have found the magic egg.
-        </p>
-        <p> Now you should <a className="App-link" href="https://www.geocent.com/careers/openings/" target="_blank" rel="noopener noreferrer" >apply for a job at Geocent</a>.
-        </p>
-      </header>
+
     </div>
   );
 }
